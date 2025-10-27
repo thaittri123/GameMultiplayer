c845191993b276838511f9a2fc307713f3339174
Tri <thaithanhtri2006@gmail.com>
2025-10-28 01:20:20 +0700

    bomb

--- AppLTMCB/Form1.Designer.cs
+++ AppLTMCB/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            button1.Location = new Point(354, 158);
+            button1.Location = new Point(594, 68);
             button1.Name = "button1";
             button1.Size = new Size(94, 26);
             button1.TabIndex = 0;
